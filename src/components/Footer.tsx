a6c9bf1a64879610df681ab7dc3ddbb80c6d6aca
Shri Krishna <skris56@uic.edu>
2024-12-22 18:39:57 -0600

    Remove LICENSE, update README demo link, rename project in package.json, and clean up unused files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,38 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import '../assets/styles/Footer.scss'
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../assets/styles/Footer.scss";
 
 function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/skris56uic" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/shri-krishna-20/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a
+          href="https://github.com/skris56uic"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/shri-krishna-20/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
       </div>
-      {/* <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p> */}
-      <p>A portfolio designed & built by Shri Krishna with 💜</p>
+      <p>
+        A portfolio designed & built by{" "}
+        <a
+          href="https://github.com/krishna-shri/personal_portfolio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Shri Krishna
+        </a>{" "}
+        with 💜
+      </p>
     </footer>
   );
 }
